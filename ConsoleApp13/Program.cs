056646981f40133b351ddc06c0713a6417fc031b
JulianaMistron <jumistron@yahoo.com.br>
2024-04-20 14:31:58 -0300

    Add project files.

---
+++ ConsoleApp13/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Digite o valor atual do salário:");
+double salarioatual = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o valor do salário com acréscimo:");
+double salarioacrescimo = double.Parse(Console.ReadLine());
+
+double aumento = salarioacrescimo - salarioatual;
+
+double porcentagemaumento = (aumento / salarioatual) * 100;
+
+Consol
